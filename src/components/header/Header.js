1809079993b440f33d396cb5fe0733067c49e131
GulBSahin <70208660+GulBSahin@users.noreply.github.com>
2021-12-05 22:12:41 +0300

    Initial commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,10 @@
+import "./Header.css"
+const Header=()=> {
+return (
+    <div className="big-title">
+        <h3> DREAM CITY ISTANBUL</h3>
+    </div>
+);
+}
+
+export default Header; 
---
+++ src/helpers/data.js
@@ -0,0 +1,94 @@
+export const data = [
+  {
+    id: 1,
+    title: "Hagia Sophia Mosque",
+    desc:
+      "The Hagia Sophia (Aya Sofya in Turkish) was the emperor's swaggering statement to the world of the wealth and technical ability of his empire. Tradition maintained that the area surrounding the emperor's throne within the church was the official center of the world.",
+    image:
+      "https://www.planetware.com/photos-large/TR/turkey-istanbul-aya-sofya.jpg",
+  },
+  {
+    id: 2,
+    title: "Topkapı Palace",
+    desc:
+      "First built by Mehmet the Conqueror in the 15th century, this glorious palace beside the Bosphorus was where the sultans of the Ottoman Empire lived and ruled from.",
+    image:
+      "https://www.planetware.com/photos-large/TR/turkey-istanbul-interior-topkapi-palace.jpg",
+  },
+  {
+    id: 3,
+    title: "Blue Mosque’",
+    desc:
+      "Sultan Ahmet I's grand architectural gift to his capital was this beautiful mosque, commonly known as the Blue Mosque today.",
+    image: "https://www.planetware.com/wpimages/2021/10/turkey-istanbul-top-attractions-visit-blue-mosque.jpg",
+  },
+  {
+    id: 4,
+    title: "Basilica Cistern",
+    desc:
+      "This huge, palace-like underground hall, supported by 336 columns in 12 rows, once stored the imperial water supply for the Byzantine emperors.",
+    image:
+      "https://www.planetware.com/photos-large/TR/turkey-istanbul-basilica-cistern.jpg",
+  },
+  {
+    id: 5,
+    title: "Stroll the Hippodrome",
+    desc:
+      "The ancient Hippodrome was begun by Septimius Severus in CE 203 and completed by Constantine the Great in CE 330. This was the center of Byzantine public life and the scene of splendid games and chariot races but also factional conflicts.",
+    image: "https://www.planetware.com/photos-large/TR/turkey-istanbul-hippodrome.jpg",
+  },
+  {
+    id: 6,
+    title: "Istanbul Archaeology Museum",
+    desc:
+      "Just a hop, skip, and jump away from Topkapı Palace, and easily visited afterwards, this important museum complex brings together an array of artifacts from both Turkey and throughout the Middle East, which sweeps through the vast breadth of history of this region.",
+    image:
+      "https://www.planetware.com/photos-large/TR/turkey-istanbul-archaeology-museum.jpg",
+  },
+  {
+    id: 7,
+    title: "Grand Bazaar",
+    desc:
+      "This massive covered market is basically the world's first shopping mall, taking up a whole city quarter, surrounded by thick walls, between the Nuruosmanıye Mosque and Beyazıt Mosque.",
+    image:
+      "https://www.planetware.com/photos-large/TR/turkey-istanbul-lantern-display-grand-bazaar.jpg",
+  },
+  {
+    id: 8,
+    title: "Süleymaniye Mosque",
+    desc:
+      "It was built for Sultan Süleyman I (commonly known as Süleyman the Magnificent; reigned 1520-1566) by the famed Ottoman architect, Sinan, who is responsible for many of the feted Ottoman-era monuments in Turkey including the Selimiye Mosque in Edirne.",
+    image: "https://www.planetware.com/photos-large/TR/turkey-istanbul-interior-suleymaniye-mosque.jpg",
+  },
+  {
+    id: 9,
+    title: "Spice Bazaar",
+    desc:
+      "The Spice Bazaar is the place to get your foodie fix of lokum (Turkish delight), dried fruit, nuts, herbs, and, of course, spices.",
+    image:
+      "https://www.planetware.com/photos-large/TR/turkey-istanbul-spice-stall-spice-bazaar.jpg",
+  },
+  {
+    id: 10,
+    title: "Dolmabahçe Palace",
+    desc:
+      "The sumptuous and ornate Dolmabahçe Palace shows the clear influence of European decoration and architecture on the Ottoman Empire in the 19th century.Built by Sultan Abdülmecid I in 1854, it replaced Topkapı Palace as the main residence of the sultans.",
+    image:
+      "https://www.planetware.com/photos-large/TR/turkey-istanbul-interior-dolmabahce-palace.jpg",
+  },
+  {
+    id: 11,
+    title: "Chora Church",
+    desc:
+      "Chora means \"country\" in Greek, and this beautiful Church (originally called the Church of St. Saviour of Chora) lay just outside old Constantinople's city walls.",
+    image: "https://www.planetware.com/photos-large/TR/turkey-istanbul-chora-church.jpg",
+  },
+  {
+    id: 12,
+    title: "Turkish Islamic Arts Museum",
+    desc:
+      "Housed in the palace of Ibrahim Paşa, who was Grand Vizier for Sultan Süleyman the Magnificent, this museum is a must-see attraction for anyone interested in Ottoman and Islamic art.",
+    image:
+      "https://www.planetware.com/photos-large/TR/turkey-istanbul-ottoman-carving-museum-of-turkish-and-islamic-arts.jpg",
+  },
+];
